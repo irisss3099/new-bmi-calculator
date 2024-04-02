c52c28623b4569949b86c8314b844b02cddc253f
irisss3099 <irisss3099@gmail.com>
2024-04-02 16:21:28 +0500

    push my bmi calculator to github

---
+++ index.ts
@@ -0,0 +1,58 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+
+console.log(chalk.red.bgWhite.bold.underline(`\n\tBMI CALCULATOR\t\n`));
+
+
+let data = await inquirer.prompt([
+    {
+        type: "list",
+        name: "gender",
+        message: "select your gender",
+        choices:["male","female"]
+    },
+    {
+        type: "number",
+        name: "age",
+        message: "write your age ="
+    }
+]);
+
+const BMI = await inquirer.prompt([
+    {
+        type: "number",
+        name: "weight",
+        message: "Enter Your weight in kg:"
+    },
+    {
+        type: "number",
+        name: "height",
+        message: "Enter Your height in meter: "
+    }
+]);
+let bmi:number = 0
+bmi = BMI.weight /(BMI.height * BMI.height)
+console.log(chalk.blueBright(`your BMI is ${bmi} kg/m2`));
+
+ 
+    if (bmi <= 18.5) {
+     console.log(chalk.cyan.bold("Underweight!!!"));   
+    }
+    else if(bmi <= 24.9) {
+        console.log(chalk.yellow.bold("Normal weight!!!")); 
+    }
+    else if (bmi <= 29.9) {
+       console.log(chalk.magenta.bold("Over weight!!!"));
+    } 
+    else  {
+       console.log(chalk.green.bold("Obesity!!!"));
+       
+    };  
+
+
+
+
+
